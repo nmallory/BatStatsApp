f0536a2e09de255429d19689be3fe3730c7e250a
Nicole Mallory <nmall76@hotmail.com>
2016-09-23 23:43:04 -0400

    Initial commit

---
+++ src/BatStatsApp.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class BatStatsApp {
+
+	public static void main(String[] args) {
+		
+		// Declare variables
+		int totalNumberBats = 0;
+	    boolean choice =true;
+		int number=0;
+		int total = 0;
+		int totalBases = 0;
+
+		
+		//Declare and Instantiate array
+		int [] hits; 
+	
+		
+		//Create scanner
+		Scanner input = new Scanner(System.in);
+		
+		//Welcome message
+		do{
+		System.out.println("Welcome to Batting Average Calculator!");
+		System.out.println();
+	
+		System.out.println("0=out, 1=single, 2=double, 3=triple, 4=home run");
+		System.out.println("===============================================");
+		
+	
+		//Prompt user to enter number of times at bat	
+		System.out.println("Enter number of times at bat:");
+		number = input.nextInt( );
+		
+		//Create new array for atBats
+		int [] atBats = new int[number];
+		
+		//Validation to ensure user only enters positive integers for number of times at bat
+		if(number< 0){
+			System.out.println("You entered a negative number. Please enter a positive number.");
+			System.out.println("Enter number of times at bat:");
+			number = input.nextInt( );
+		}
+		
+		//Creates new object
+		hits=new int[number];
+	
+
+		//Displays number of at bats
+		for (int i = 0; i < hits.length; i++) {
+			total = total + 1 ;
+			System.out.print("Result for at-bat "  + i +  ": " );
+			
+			
+			//User input times at bat
+			hits[i] = input.nextInt();
+			
+			//Validation to ensure user enters a number between 0 to 4
+		
+			if(hits[i] > 4 && hits[i]<0){
+			
+				System.out.println("You entered an invalid choise. Please enter a number between 0 and 4.");
+				hits[i] = input.nextInt();
+			}
+		}
+			
+		//Calculate number of hits and sum of bases
+		for(int i : atBats) {
+			if(i > 0)
+				hits[i]++;
+				total += i;
+		}
+	
+		//Calculate battingAve and slugPercent
+		double slug = (double) total/ number;
+		double battingAvg = (double) number/ total;
+		
+		//Prints out batting average
+		System.out.println("Batting average:" +battingAvg);
+		
+		//Prints out slugging percentage
+		System.out.println("Slugging Percentage:" +slug);
+	
+		//Prompt user to another another batter if they want to continue
+		System.out.println("Another batter? (y/n):");
+		input.next( );
+		
+	
+		//Validate to ensure user only enters a "Y", "y", "N", or "n"
+		
+		
+		//While loop to allow user to return to the beginning of program
+		}while(true);
+	}
+
+}
